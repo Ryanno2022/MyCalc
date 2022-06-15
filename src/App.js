a46795d9cb533f4e82dc624a209437a7625fdaaa
Ryanno2022 <danieljryan02@gmail.com>
2022-06-15 15:13:44 +0100

    Did a complete rewrite of every component to change them from class-based to functional components. Added a module.css for each file. Created a switch case for App.js and added state. Outsourced the buttons into a new component called ButtonGrid and added previous styling from App.js. Created a bug where the usual functionality of assigning operator values through isOperator in Button.js has been blocked. Will need to create a clickHandler with internal div to solve this.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from 'App.module.css';
+import { useState } from 'react';
+import ButtonGrid from './components/ButtonGrid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  let [input, setInput] = useState('');
+  let [previousNumber, setPreviousNumber] = useState('');
+  let [currentNumber, setCurrentNumber] = useState('');
+  let [operator, setOperator] = useState('');
+
+
+  const addToInput = (val) => {
+    setInput(input + val);
+  }
+
+  const addDecimal = (val) => {
+    if(input.indexOf('.') === -1) {
+      setInput(input + val);
+    }
+  }
+
+  const addZeroToInput = (val) => {
+    if(input !==('')) {
+      setInput(input + val);
+    }
+  }
+
+  const clearInput = () => {
+    setInput("")
+    setOperator() // Have another look at this
+  }
+
+  const add = () => {
+    input = setInput(input);
+    previousNumber = setPreviousNumber(previousNumber + "+");
+    setInput('');
+    setOperator('plus');
+  }
+
+  const subtract = () => {
+    input = setInput(input);
+    previousNumber = setPreviousNumber(previousNumber + "-");
+    setInput('');
+    operator = setOperator('subtract');
+  }
+
+  const multiply = () => {
+    input = setInput(input);
+    previousNumber = setPreviousNumber(previousNumber + "x");
+    setInput('');
+    operator = setOperator('multiply');
+  }
+
+  const divide = () => {
+    input = setInput(input);
+    previousNumber = setPreviousNumber(previousNumber + "/");
+    setInput('');
+    setOperator('divide');
+  }
+
+  const square = () => {
+    input = setInput(input);
+    previousNumber = setPreviousNumber(previousNumber + "^2");
+    setInput('');
+    setOperator('square');
+  }
+
+  const cube = () => {
+    input = setInput(input);
+    previousNumber = setPreviousNumber(previousNumber + "^3");
+    setInput('');
+    setOperator('cube');
+  }
+  
+  const squareroot = () => {
+    input = setInput(input);
+    previousNumber = setPreviousNumber(previousNumber + "^1/2");
+    setInput('');
+    setOperator('squareroot');
+  }
+
+  const tenfold = () => {
+    input = setInput(input);
+    previousNumber = setPreviousNumber(previousNumber + "(x10)");
+    setInput('');
+    setOperator('tenfold');
+  }
+ 
+  const evaluate = () => {
+    currentNumber = setCurrentNumber(input);
+    switch(operator) {
+      case "plus":
+        add();
+        setInput(parseInt(previousNumber) + parseInt(currentNumber));
+        break;
+      case "subtract":
+        subtract();
+        setInput(parseInt(previousNumber) - parseInt(currentNumber));
+        break;
+
+      case"multiply":
+        multiply();
+        setInput(parseInt(previousNumber) * parseInt(currentNumber));
+        break;
+
+      case "divide":
+        divide();
+        setInput(parseInt(previousNumber) / parseInt(currentNumber));
+        break;
+
+      case"square":
+        square();
+        setInput(parseInt(previousNumber)) * setInput(parseInt(previousNumber));
+        break;
+
+      case"cube":
+        cube();
+        let x = setInput(parseInt(previousNumber));
+        setInput(x * x * x);
+        break;
+
+      case"squareroot":
+        squareroot();
+        setInput(Math.sqrt(parseInt(previousNumber)));
+        break;
+
+      case"tenfold":
+        tenfold();
+        setInput(parseInt(previousNumber) * 10);
+        break;
+      case"default":
+        setOperator("");
+        break;
+    }
+  
+
+    return (
+      <div className={classes.app}>
+        <ButtonGrid />
+      </div>
+    );
+  }
 }
 
 export default App;
+  
+
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+// import { Fragment } from 'react';
+// import './Header.css';
+// import logo from './assets/logo.svg';
+
+// export default function Header() {
+
+
+//     return (
+//         <Fragment>
+//         <div>
+//             <div className="header" />
+//                     <img src={logo} className="logo" alt="logo" />
+//         </div>
+//         </Fragment>
+//     );
+// }
